1a0549e60e93d8e4d12a5c0d7763cda98313d681
khain777 <miguelansemil@gmail.com>
2015-03-18 11:40:44 +0100

    Submit file

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
@@ -12,4 +24,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(!is.null(x$getsolve())){
+                message("getting cached data")
+                return(x$getsolve())
+        }else if(!is.matrix(x$get())){
+                message("The object must be a matrix")
+                return(NA);
+        }else if(ncol(x$get()) != nrow(x$get())){
+                message("The matrix must be square")
+                return(NA);
+        }else if(!is.numeric(x$get())){
+                message("The matrix must be numeric")
+                return(NA);
+        }
+        
+        x$setsolve(solve(x$get(), ...))
+        
+        x$getsolve()
 }
